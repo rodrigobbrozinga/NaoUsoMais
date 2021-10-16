0d8c94a3d1b273ca8ca7746795ac29d27e9e1e40
rodrigobbrozinga <rodrigobbrozinga@gmail.com>
2021-10-16 16:00:46 -0300

    Primeiro commit

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NaoUsoMais.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public IActionResult Catalogo()
+        {
+            return View();
+        }
+
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+
+        public IActionResult Resumo()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
