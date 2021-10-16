5eced7d9868abade51178585005f89ed1decc0c8
rodrigobbrozinga <rodrigobbrozinga@gmail.com>
2021-10-16 16:48:16 -0300

    Rota padrão e rodapé

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace NaoUsoMais
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Pedido}/{action=Catalogo}/{id?}");
             });
         }
     }
